bc00fa8c46a45f03e091acceb9e9a499253c8d94
gabriel <gabriel.andres26@gmail.com>
2018-03-05 00:25:05 -0400

    PArt 2 Done

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,51 +1,62 @@
 <template>
-  <div>
-    <h1>Register</h1>
-
-     <input
-     type="email"
-     name="email"
-     v-model="email"
-     placeholder="email"/>
-     <br>
-     <input
-     type="password"
-     name="password"
-     v-model="password"
-     placeholder="password"/>
-<br>
-<div v-html="error"/>
-<button @click="register">Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+          <div class="pl-4 pr-4 pt-2 pb-2">
+            <input
+              type="email"
+              name="email"
+              v-model="email"
+              placeholder="email"/>
+              <br>
+              <input
+              type="password"
+              name="password"
+              v-model="password"
+              placeholder="password"/>
+              <br>
+              <div class="error" v-html="error"/>
+              <br>
+              <v-btn class="cyan" @click="register">Register
+              </v-btn>
+          </div>   
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import AuthenticationService from '@/service/AuthenticationService'
+import AuthenticationService from "@/service/AuthenticationService";
 export default {
-  data () {
+  data() {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       error: null
-    }
+    };
   },
 
   methods: {
-    async register () {
+    async register() {
       try {
         await AuthenticationService.register({
           email: this.email,
           password: this.password
-        })
+        });
       } catch (error) {
-        this.error = error.response.data.error
+        this.error = error.response.data.error;
       }
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error {
+  color: red;
+}
 </style>
