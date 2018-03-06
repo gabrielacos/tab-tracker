a36836c2114a0346a64e55d13d96e291b61d8653
gabriel <gabriel.andres26@gmail.com>
2018-03-05 23:04:22 -0400

    changes

---
+++ client/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-toolbar fixed class="cyan" dark>
+    <v-toolbar-title class="mr-4">
+      Tab Tracker  
+    </v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn router to="/" flat dark>
+        Home 
+      </v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+     <v-toolbar-items>
+      <v-btn router to="login"  flat dark>
+       Login
+      </v-btn>
+    </v-toolbar-items>
+      <v-toolbar-items>
+        <v-btn router to="register" flat dark>
+          Sign Up
+        </v-btn>
+      </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,7 +3,7 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
           <div class="pl-4 pr-4 pt-2 pb-2">
             <v-text-field
@@ -20,7 +20,7 @@
               <br>
               <div class="error" v-html="error"/>
               <br>
-              <v-btn class="cyan" dark @click="register">Register
+              <v-btn class="cyan" dark @click="login">Login
               </v-btn>
           </div>   
       </div>
@@ -42,9 +42,9 @@ export default {
   },
 
   methods: {
-    async register() {
+    async login() {
       try {
-        await AuthenticationService.register({
+        await AuthenticationService.login({
           email: this.email,
           password: this.password
         });
@@ -61,4 +61,6 @@ export default {
 .error {
   color: red;
 }
+
+
 </style>
